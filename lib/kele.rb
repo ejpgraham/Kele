b540604e678c1b270d115efa15c0a8b983e4c2c8
ejpgraham <ejpgraham@gmail.com>
2018-02-10 22:48:43 -0500

    Kele uses JSON gem to parse response from bloc into a ruby object.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,22 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    @api_url = "https://www.bloc.io/api/v1"
-    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { email: email, password: password })
-    p response.code
+    response = self.class.post(api_url("/sessions"), body: { email: email, password: password })
     raise 'Invalid username or password.' if response.code == 401
     @auth_token = response["auth_token"]
+  end
+  
+  def get_me
+    response = self.class.get(api_url("/users/me"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+  
+  def api_url(destination)
+    "https://www.bloc.io/api/v1" + destination
   end
 
 end
