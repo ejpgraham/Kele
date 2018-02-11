b91b8db74356e59a01d1266f88be8740fcd3a4fa
ejpgraham <ejpgraham@gmail.com>
2018-02-11 18:28:18 -0500

    Kele sends and retrieves messages.

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,13 +14,36 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(api_url("/users/me"), headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("/users/me"), headers: { authorization: @auth_token })
     @my_info_hash = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id = @my_info_hash["current_enrollment"]["mentor_id"])
-    response = self.class.get(api_url("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("/mentors/#{mentor_id}/student_availability"), headers: { authorization: @auth_token })
     JSON.parse(response.body)
+  end
+
+  def get_messages(page_number = nil)
+    if page_number
+      response = self.class.get(api_url("/message_threads"), headers: { authorization: @auth_token }, body: { page: page_number})
+    else
+      response = self.class.get(api_url("/message_threads"), headers: { authorization: @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+
+  def create_message(sender_email, recipient_id, subject = "No Subject", message_body )
+    response = self.class.post(api_url("/messages"), {
+      headers: { authorization: @auth_token },
+      body: {
+        sender: sender_email,
+        recipient_id: recipient_id,
+        subject: subject,
+        :"stripped-text" => message_body
+      }
+    })
+    p "Message Sent!"  if response.success?
   end
 
   def api_url(destination)
