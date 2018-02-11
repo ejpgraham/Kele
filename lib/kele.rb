b3f20142e882a1d28be330cbcb577eb63501c8ae
ejpgraham <ejpgraham@gmail.com>
2018-02-11 16:46:00 -0500

    Gem now retrieves and parses mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,18 +3,25 @@ require 'json'
 
 class Kele
   include HTTParty
+  attr_reader :my_info_hash, :auth_token
 
   def initialize(email, password)
     response = self.class.post(api_url("/sessions"), body: { email: email, password: password })
     raise 'Invalid username or password.' if response.code == 401
     @auth_token = response["auth_token"]
   end
-  
+
   def get_me
     response = self.class.get(api_url("/users/me"), headers: { "authorization" => @auth_token })
+    @my_info_hash = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability
+    mentor_id = @my_info_hash["current_enrollment"]["mentor_id"]
+    response = self.class.get(api_url("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
-  
+
   def api_url(destination)
     "https://www.bloc.io/api/v1" + destination
   end
